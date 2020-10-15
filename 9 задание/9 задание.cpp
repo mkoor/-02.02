8c2ee0f1b191aefbc095652ee2e06028f3ea5cbb
Михаил Кораблев <cstud110917@vyatsu.ru>
2020-10-15 20:06:04 +0300

    new code

--- 9 задание/9 задание.cpp
+++ 9 задание/9 задание.cpp
@@ -1,15 +1,30 @@
 ﻿#include <iostream>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
 	int a[10] = {};
+	int length = 10;
+	int n;
+
 	srand(time(NULL));
+
 	for (int i = 0; i < 10; i++)
 	{
 		a[i] = rand() % 10;
 		cout << a[i] << " ";
 	}
+	cout << "\n";
+
+	for (int i = 0; i < length / 2; i++)
+	{
+		n = a[i];
+		a[i] = a[(length - 1) - i];
+		a[length - 1 - i] = n;
+	}
+	for (int i = 0; i < length; ++i) cout << a[i] << ' ';
 	cout << endl;
+	return 0;
 }
